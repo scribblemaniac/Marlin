6bcb41226eeb234fde9f3b1194eb352c59e73b7a
scribblemaniac <scribblemaniac@users.noreply.github.com>
2024-01-06 11:39:54 -0700

    Add CR Touch configuration

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1510,7 +1510,7 @@
  *  - The difference is used to set the probe Z offset.
  */
 #if HAS_BED_PROBE && ANY(HAS_MARLINUI_MENU, HAS_TFT_LVGL_UI)
-  //#define PROBE_OFFSET_WIZARD
+  #define PROBE_OFFSET_WIZARD
   #if ENABLED(PROBE_OFFSET_WIZARD)
     /**
      * Enable to init the Probe Z-Offset when starting the Wizard.
